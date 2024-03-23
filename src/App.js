ef6c33f68709426c57eb693a4815da0790d5ce2d
filiperak <filip.erak@googlemail.com>
2024-03-23 18:46:25 +0100

    npm,sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>lets build</h1>
+      <Sidebar/>
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import './Sidebar.css'
+import TwitterIcon from '@mui/icons-material/Twitter';
+const Sidebar = () => {
+    return (
+        <div>
+            <TwitterIcon/>
+        </div>
+    );
+}
+ 
+export default Sidebar;
