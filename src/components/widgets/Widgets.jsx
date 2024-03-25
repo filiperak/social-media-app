b45eaec743fada518685eb91a153f45a08c3637b
filiperak <filip.erak@googlemail.com>
2024-03-25 18:19:06 +0100

    widgets

--- src/components/widgets/Widgets.jsx
+++ src/components/widgets/Widgets.jsx
@@ -1,3 +1,4 @@
+import { Calculate } from '@mui/icons-material';
 import './Widgets.css';
 import SearchIcon from '@mui/icons-material/Search';
 import { TwitterTimelineEmbed,TwitterShareButton,TwitterTweetEmbed } from 'react-twitter-embed';
@@ -11,7 +12,7 @@ const Widgets = () => {
             <div className="widgets-widget-container">
                 <h2>What's Happening</h2>
             <TwitterTweetEmbed tweetId={'1771622119715000357'}/>
-            <TwitterTimelineEmbed sourceType='profile' screenName='VladoGeorgiev' options={{ height: 400 }}/>
+            <TwitterTimelineEmbed sourceType='profile' screenName='VladoGeorgiev' options={{ height: 250 }}/>
             <TwitterShareButton
              url={"https://facebook.com"}
             options={{ text: "test", via: "test" }}
